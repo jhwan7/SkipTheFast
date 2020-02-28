70029aa55b9c98bb83a48adeef303771992170ae
hpark0811 <hpark327@uwo.ca>
2020-02-28 00:32:59 -0500

    add exercise monitor fragments

--- app/src/main/java/com/example/skipthefast/JourneyFragment.kt
+++ app/src/main/java/com/example/skipthefast/JourneyFragment.kt
@@ -37,6 +37,10 @@ class JourneyFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_journey, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
     companion object {
         /**
          * Use this factory method to create a new instance of
