048ddd8a147604977b061e767e3bc1a9986ec155
Mathieu Girard <mathieu.m.girard@gmail.com>
2020-03-04 14:00:25 -0500

    added write and read all

--- app/src/main/java/com/example/skipthefast/MainActivity.kt
+++ app/src/main/java/com/example/skipthefast/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.skipthefast
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.example.skipthefast.server.FBServer
@@ -9,9 +11,12 @@ import com.example.skipthefast.ui.main.SectionsPagerAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var ref :  DatabaseReference
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,11 +26,25 @@ class MainActivity : AppCompatActivity() {
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
         val fab: FloatingActionButton = findViewById(R.id.fab)
-        var fbServer = FBServer()
-        fbServer.connect("https://test-projec-5a491.firebaseio.com/records.json")
+        var fbServer = FBServer();
         fab.setOnClickListener {
             val intent = Intent(this, FormActivity::class.java)
             startActivity(intent)
         }
+
+        ref = FirebaseDatabase.getInstance().getReference("records/mathieu")
+        ref.addValueEventListener(object: ValueEventListener {
+            override fun onCancelled(p0: DatabaseError){
+                TODO("not implemented")
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                if(p0!!.exists()){
+                   for(h in p0.children){
+                       println(h.value)
+                   }
+                }
+            }
+        })
     }
 }
--- app/src/main/java/com/example/skipthefast/server/FBServer.kt
+++ app/src/main/java/com/example/skipthefast/server/FBServer.kt
@@ -1,25 +1,38 @@
 package com.example.skipthefast.server
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.google.firebase.database.*
 import okhttp3.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.io.IOException
 
 class FBServer {
+
     private val client = OkHttpClient()
+    private val database = FirebaseDatabase.getInstance().reference
 
-    fun connect(url: String) {
-        val request = Request.Builder().url(url).build()
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                print("FAILED CALL ON ")
-                println(url)
-                println(e)
-            }
-            override fun onResponse(call: Call, response: Response) {
-                print("Successful call on ")
-                println(url)
-                println(response.body()?.string())
-            }
-        })
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getTime(): String? {
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        return current.format(formatter)
+    }
 
+    @IgnoreExtraProperties
+    data class Entry(
+        var Date: String? = "",
+        var Category: String? = "",
+        var Feeling: Int? = 0,
+        var FoodChain: String? = "",
+        var Item: String? = "",
+        var Price: Double? = 0.00
+    )
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun writeEntry(user: String, category: String, feeling: Int, foodChain: String, item: String, price: Double){
+        val entry = Entry(getTime(), category, feeling, foodChain, item, price)
+        database.child("records").child(user).push().setValue(entry)
     }
 }
