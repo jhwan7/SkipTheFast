7a7914112c4f2883c874d47c0c8cbc09966dd472
rsongdalsong <jeongwon412@gmail.com>
2020-03-04 15:42:12 -0500

    Added getRecords func definition

--- app/src/main/java/com/example/skipthefast/server/FBServer.kt
+++ app/src/main/java/com/example/skipthefast/server/FBServer.kt
@@ -6,11 +6,11 @@ import com.google.firebase.database.*
 import okhttp3.*
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.io.IOException
+//import java.io.IOException
 
 class FBServer {
 
-    private val client = OkHttpClient()
+    //private val client = OkHttpClient()
     private val database = FirebaseDatabase.getInstance().reference
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -34,5 +34,10 @@ class FBServer {
     fun writeEntry(user: String, category: String, feeling: Int, foodChain: String, item: String, price: Double){
         val entry = Entry(getTime(), category, feeling, foodChain, item, price)
         database.child("records").child(user).push().setValue(entry)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getRecords(user:String){
+        // TODO('implements')
     }
 }
