657d08ee4a65472379a348f6916ca4cb35f94906
rsongdalsong <jeongwon412@gmail.com>
2020-03-05 14:48:02 -0500

    Reorganized files

--- server/Firebase/__init__.py
+++
@@ -1,4 +0,0 @@
-# __init__.py
-from CONST import FB_CONFIG
-import pyrebase
-firebase = pyrebase.initialize_app(FB_CONFIG)
--- server/server.py
+++ server/server.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, send_file
-# import Firebase
 
 app = Flask(__name__)
 
