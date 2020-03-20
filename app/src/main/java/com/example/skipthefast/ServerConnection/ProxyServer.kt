0bacce709bb85dae7c8253733e42d7a69c055067
rsongdalsong <jeongwon412@gmail.com>
2020-03-19 23:32:20 -0400

    IP Changed to avoid merge conflict

--- app/src/main/java/com/example/skipthefast/ServerConnection/ProxyServer.kt
+++ app/src/main/java/com/example/skipthefast/ServerConnection/ProxyServer.kt
@@ -8,7 +8,7 @@ import org.json.JSONObject
 import java.io.IOException
 
 open class ProxyServer {
-    protected val SERVER_URL = "http://35.183.208.183:8000"
+    protected val SERVER_URL = "http://99.79.73.40:8000"
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun testConnection(){
